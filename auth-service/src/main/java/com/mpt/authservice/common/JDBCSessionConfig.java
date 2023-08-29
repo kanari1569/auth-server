ac17640528d3bdaff33467b3f73d90272e3c550a
root <root@DESKTOP-2LNIRDJ>
2023-08-30 04:45:20 +0900

    db session test

--- auth-service/src/main/java/com/mpt/authservice/common/JDBCSessionConfig.java
+++ auth-service/src/main/java/com/mpt/authservice/common/JDBCSessionConfig.java
@@ -5,10 +5,12 @@ import javax.sql.DataSource;
 // import jakarta.activation.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Configuration
 @EnableJdbcHttpSession
 public class JDBCSessionConfig{
 
