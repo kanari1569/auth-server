d183d34655eef97de532fab6bac09172f5282dd2
LeeJusuk208 <100745723+LeeJusuk208@users.noreply.github.com>
2023-09-04 02:43:39 +0900

    Update gradle_build.sh

--- gradle_build.sh
+++ gradle_build.sh
@@ -1,10 +1,10 @@
 # make gradle zip files
-zip -s 0 ./auth-service/gradle/wrapper/gradle-8.1.1.zip --out ./auth-service/gradle/wrapper/gradle-8.1.1-bin.zip
+# zip -s 0 ./auth-service/gradle/wrapper/gradle-8.1.1.zip --out ./auth-service/gradle/wrapper/gradle-8.1.1-bin.zip
 
 # build stage
-cd ./auth-service
-./gradlew clean build --stacktrace
+# cd ./auth-service
+./auth-service/gradlew clean build --stacktrace
 
 # clear temp files 
-cd ..
-rm ./auth-service/gradle/wrapper/gradle-8.1.1-bin.zip
+# cd ..
+# rm ./auth-service/gradle/wrapper/gradle-8.1.1-bin.zip
